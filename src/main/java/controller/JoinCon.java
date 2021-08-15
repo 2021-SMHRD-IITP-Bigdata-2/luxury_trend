9bb18b1b47c13e322f02dd60a6a0019180bba217
김혜진 <71439666+momoIllsam@users.noreply.github.com>
2021-08-15 23:23:24 +0900

    로그인콘..!
    경로수정

--- src/main/java/controller/JoinCon.java
+++ src/main/java/controller/JoinCon.java
@@ -1,7 +1,45 @@
-                    </li>
-                    <li class="nav-item" data-toggle="chart" data-target="#chart-sales" data-update='{"data":{"datasets":[{"data":[0, 20, 5, 25, 10, 30, 15, 40, 40]}]}}' data-prefix="$" data-suffix="k">
-                      <a href="#" class="nav-link py-2 px-3" data-toggle="tab">
-                        <span class="d-none d-md-block">Week</span>
-                        <span class="d-md-none">W</span>
-                      </a>
-                    </li>
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import user.UserDAO;
+import user.UserDTO;
+
+@WebServlet("/JoinCon")
+public class JoinCon extends HttpServlet {
+
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      request.setCharacterEncoding("EUC-KR");
+
+         String email = request.getParameter("email");
+         String pw = request.getParameter("pw");
+         String name = request.getParameter("name");
+
+            UserDAO dao = new UserDAO();
+            UserDTO User = new UserDTO(email, pw, name);
+            
+            int cnt = dao.user_join(User);
+            
+            System.out.println(cnt);
+            
+            if(cnt>0) {
+               
+               RequestDispatcher dispatcher 
+                     = request.getRequestDispatcher("index.jsp"); 
+              // request
+               request.setAttribute("email", email);
+               // )
+               dispatcher.forward(request, response);
+//               response.sendRedirect("join_success.jsp?email="+email);
+            }else {
+               response.sendRedirect("/LuxuryPratice2/examples/login.jsp");
+            }
+         } 
+   }
